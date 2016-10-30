f496b5709dcf6ad4308bcd55a465cdb59eadfbba
Liam England <Liam England>
2016-10-30 15:52:23 +0000

    Initial commit

---
+++ MiniTextEditor/Mini Text Editor.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MiniTextEditor
+{
+    partial class MiniTextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.rtxAll = new System.Windows.Forms.RichTextBox();
+            this.tmrAutoShadow = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(3, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(99, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(241, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(121, 3);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(99, 23);
+            this.btnFont.TabIndex = 2;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // rtxAll
+            // 
+            this.rtxAll.Location = new System.Drawing.Point(3, 33);
+            this.rtxAll.Name = "rtxAll";
+            this.rtxAll.Size = new System.Drawing.Size(337, 241);
+            this.rtxAll.TabIndex = 3;
+            this.rtxAll.Text = "";
+            // 
+            // tmrAutoShadow
+            // 
+            this.tmrAutoShadow.Enabled = true;
+            this.tmrAutoShadow.Tick += new System.EventHandler(this.tmrAutoShadow_Tick);
+            // 
+            // MiniTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 277);
+            this.Controls.Add(this.rtxAll);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.MaximizeBox = false;
+            this.Name = "MiniTextEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Text Editor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.RichTextBox rtxAll;
+        private System.Windows.Forms.Timer tmrAutoShadow;
+    }
+}
+
---
+++ MiniTextEditor/Mini Text Editor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MiniTextEditor
+{
+    public partial class MiniTextEditor : Form
+    {
+        bool bolSaved;
+
+        public MiniTextEditor()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle; // This stops the user from resizing the box.
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog(); // Creates method called 'fileDialog' which opens up the file explorer when called.
+            fileDialog.ShowDialog(); // Opens the file dialog.            
+
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && fileDialog.FileName.Contains(".txt")) // Makes sure that everything is ok, as well as the filename contains .txt so can be used within the editor.
+            {
+                string openFile = File.ReadAllText(fileDialog.FileName); // This reads in all text from the selected file.
+                rtxAll.Text = openFile;
+            }
+
+            else
+            {
+                MessageBox.Show(""); // If something goes wrong, the messagebox will appear.
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.ShowDialog(); //Shows the font dialog.
+            if (fontDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                rtxAll.Font = fontDialog.Font; // Sets the font of the text box to what's selected in font dialog.
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string name = saveFileDialog.FileName + ".txt"; // Making sure the file extension is '.txt'
+                File.WriteAllText(name, rtxAll.Text); //Write all text in the texbox into the file.
+                bolSaved = true;
+            }
+        }
+
+        private void tmrAutoShadow_Tick(object sender, EventArgs e)
+        {
+            if (bolSaved == true)
+            {
+                // This is where I'll start implementing the autosaving.
+            }
+
+            else
+            {
+                
+            }
+        }
+    }
+}
